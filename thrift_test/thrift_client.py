6497370e52450d09c599aafe6db1b8717bd6eec0
Young5566 <403353323@qq.com>
2019-09-27 14:31:14 +0800

    test

---
+++ thrift_test/thrift_client.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname('__file__'), os.path.pardir)))
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TCompactProtocol
+from test.PersonService import Client
+from test.PersonService import Person
+
+__HOST = 'localhost'
+__PORT = 8899
+
+try:
+    tsocket = TSocket.TSocket(__HOST, __PORT)
+    transport = TTransport.TFramedTransport(tsocket)
+    protocol = TCompactProtocol.TCompactProtocol(transport)
+    client = Client(protocol)
+    transport.open()
+    data = Person("list")
+    print('client-requets')
+    res = client.getPersonByUsername("lisi")
+    # print(client.do_format(data).text)
+    print('server-answer', res)
+
+    transport.close()
+except Thrift.TException as ex:
+    print(ex.message)
